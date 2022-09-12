9af8088404b7a64249d4ac34bfa5e818fcbccf75
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-09-12 18:33:13 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,72 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './components/Loading';
 
 class Login extends Component {
+  state = {
+    name: '',
+    isEnterButtonDisabled: true,
+    loading: false,
+    redirect: false,
+  };
+
+  handleClick = () => {
+    this.setState({ loading: true }, async () => {
+      const { name: param } = this.state;
+      await createUser({ name: param });
+      this.setState({ loading: false, redirect: true });
+    });
+  };
+
+  enableButtonSave = () => {
+    const { name } = this.state;
+    if (name.length > 2) {
+      this.setState({ isEnterButtonDisabled: false });
+    }
+  };
+
+  handleOnChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => {
+      this.enableButtonSave();
+    });
+  };
+
   render() {
-    return <div data-testid="page-login">Login</div>;
+    const { name, loading, redirect, isEnterButtonDisabled } = this.state;
+    return (
+      <div>
+        {redirect ? (
+          <Redirect to="/search" />
+        ) : (
+          <div>
+            {loading ? (
+              <Loading />
+            ) : (
+              <div data-testid="page-login">
+                <label htmlFor="name">
+                  <input
+                    name="name"
+                    value={ name }
+                    data-testid="login-name-input"
+                    id="name"
+                    onChange={ this.handleOnChange }
+                  />
+                </label>
+                <button
+                  type="button"
+                  disabled={ isEnterButtonDisabled }
+                  data-testid="login-submit-button"
+                  onClick={ this.handleClick }
+                >
+                  Entrar
+                </button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
   }
 }
 
